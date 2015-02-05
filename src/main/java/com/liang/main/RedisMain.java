c32005b6be5e0d1b93e6095bdf0d56f91f1b70a8
981233589@qq.com <Angel1205>
2015-02-05 17:50:19 +0800

    init new code
    init new code

---
+++ src/main/java/com/liang/main/RedisMain.java
@@ -0,0 +1,22 @@
+package com.liang.main;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Administrator
+ *
+ */
+public class RedisMain {
+
+	/**
+	 * 日志记录器
+	 */
+	private static Logger logger = Logger.getLogger(RedisMain.class);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.info("Spring+Redis的Main...启动!");
+	}
+
+}
